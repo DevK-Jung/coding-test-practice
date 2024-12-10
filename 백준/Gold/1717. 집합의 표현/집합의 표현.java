3c3199c56ad9863464dbae7ded77c6a633d95f97
JUNGHYEON KIM <dev.kjung@gmail.com>
2024-12-10 22:48:36 +0900

    [Gold V] Title: 집합의 표현, Time: 4112 ms, Memory: 269756 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[] arr;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        arr = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            int type = sc.nextInt();
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            if (type == 0) {
+                union(a, b);
+            } else {
+                int c = find(a);
+                int d = find(b);
+
+                System.out.println(c == d ? "YES" : "NO");
+            }
+        }
+    }
+
+    private static int find(int target) {
+        if (arr[target] == target) return target;
+
+        return find(arr[target]);
+    }
+
+    private static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if(b != a) arr[b] = a;
+
+
+    }
+}
